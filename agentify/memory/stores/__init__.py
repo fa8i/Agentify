580e646bfd044e897ac7acba3186769382f21781
Fabian M <159306367+fa8i@users.noreply.github.com>
2025-12-22 00:47:44 +0100

    add SQLiteStore implementation

--- agentify/memory/stores/__init__.py
+++ agentify/memory/stores/__init__.py
@@ -2,8 +2,9 @@
 from .in_memory_store import InMemoryStore
 from .redis_store import RedisStore
 from .elastic_store import ElasticsearchStore
+from .sqlite_store import SQLiteStore
 
-__all__ = ["InMemoryStore", "RedisStore", "ElasticsearchStore"]
+__all__ = ["InMemoryStore", "RedisStore", "ElasticsearchStore", "SQLiteStore"]
 
 
 
